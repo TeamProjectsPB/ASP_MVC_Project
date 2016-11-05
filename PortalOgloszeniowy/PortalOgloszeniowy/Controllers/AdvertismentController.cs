9d79eee18e1e1891c9f804e6de6f97994fb83d6c
mARCIN <Dell@MARCIN_DELL_747>
2016-11-05 13:34:09 +0100

    Dodałem strone z widokiem ogłoszenia

---
+++ PortalOgloszeniowy/PortalOgloszeniowy/Controllers/AdvertismentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortalOgloszeniowy.Controllers
+{
+    public class AdvertismentController : Controller
+    {
+        // GET: Advertisment
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
